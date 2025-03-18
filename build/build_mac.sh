a8785ad09514fa4f517d7a2ba4f6378f65de72a1
MEL <15801230835@163.com>
2025-03-18 18:29:16 +0800

    [add file]提交 mac 平台的编译脚本

---
+++ build/build_mac.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+# Step 1: Clean old build and install directories
+echo "Cleaning old build and install directories..."
+
+cd ..
+if [ -d "build-release" ]; then
+    rm -rf build-release
+fi
+
+if [ -d "build-debug" ]; then
+    rm -rf build-debug
+fi
+
+if [ -d "build_mac_release" ]; then
+    rm -rf build_mac_release
+fi
+
+if [ -d "build_mac_debug" ]; then
+    rm -rf build_mac_debug
+fi
+
+# Step 2: Build release version
+echo "Building Release version..."
+
+mkdir build-release
+cd build-release
+
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../build_mac_release -DCMAKE_OSX_ARCHITECTURES=x86_64
+
+make
+make install
+
+cd ..
+
+# Step 3: Build debug version
+echo "Building Debug version..."
+
+mkdir build-debug
+cd build-debug
+
+cmake .. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../build_mac_debug -DCMAKE_OSX_ARCHITECTURES=x86_64
+make
+make install
+
+cd ..
+
+echo "Build process completed!"
